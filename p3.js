bc962cef6dccfbc97c451ec78e4032ad65f381c1
LakeCelo <hikucelo23@gmail.com>
2023-02-02 14:13:38 -0300

    initial commit

---
+++ p3.js
@@ -0,0 +1,42 @@
+
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
+
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+function add() {
+
+}
+function save() {
+  localStorage.setItem('Tulim@habits', JSON.stringify(nlwSetup.data))
+}
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert("Dia já incluso ❌")
+    return
+  }
+
+  alert('Adicionado com sucesso ✔️')
+  nlwSetup.addDay(today)
+}
+const data = JSON.parse(localStorage.getItem("Tulim@habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
+//const data = {
+// run: ["01-23", "01-24", "01-25", "01-26", "01-27", "01-28", "01-29", "01-30", "01-31"],
+//  shower: ["01-23", "01-24", "01-25", "01-26", "01-27", "01-28", "01-29", "01-30", "01-31"],
+//  playing: ["01-23", "01-24", "01-25", "01-26", "01-27", "01-28", "01-29", "01-30", "01-31"],
+//  task: ["01-23", "01-24", "01-25", "01-26", "01-27", "01-28", "01-29", "01-30", "01-31"],
+//  sleep: ["01-23", "01-24", "01-25", "01-26", "01-27", "01-28", "01-29", "01-30", "01-31"],
+// }
+
+nlwSetup.setData(data)
+nlwSetup.load()
+
+
